87f0a922f004179b79b19a4e76be9b84d9d15602
duya25446 <i145463912@yahoo.com>
2023-10-09 23:34:39 +0800

    update

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -5,9 +5,13 @@
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// TODO: implement the message variant types based on their usage below
 
 enum Message {
+    ChangeColor(u8, u8, u8),
+    Echo(String),
+    Move(Point),
+    Quit
     // TODO: implement the message variant types based on their usage below
 }
 
@@ -39,6 +43,14 @@ impl State {
     }
 
     fn process(&mut self, message: Message) {
+        match  message{
+            Message::ChangeColor(r,g,b) => self.change_color((r,g,b)),
+            Message::Echo(st) => self.echo(st),
+            Message::Move(mv) => self.move_position(mv),
+            Message::Quit => self.quit()
+            
+        }
+
         // TODO: create a match expression to process the different message
         // variants
         // Remember: When passing a tuple as a function argument, you'll need
